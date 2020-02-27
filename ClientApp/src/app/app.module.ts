3a4ee9da4a2db04e1debbb8853f587ce049516a7
kzmijak <kzmijak@gmail.com>
2020-02-27 20:53:57 +0100

    Day 5 Progress Log
    Changed visual structure of the website
    Created Movie Table component
    Created Movie Details component
    Implemented routing configuration

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ModelModule } from "./models/model.module";
+import { MovieTableComponent } from "./structure/movieTable.component";
+import { CategoryFilterComponent } from "./structure/categoryFilter.component";
+import { MovieDetailComponent } from './structure/movieDetail.component';
+import { RoutingConfig } from "./structure/app.routing";
 
 @NgModule({
   declarations: [
+    MovieTableComponent,
+    CategoryFilterComponent,
+    MovieDetailComponent,
     AppComponent
   ],
   imports: [
+    RoutingConfig,
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     ModelModule,
